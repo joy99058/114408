ca7a1b72e66748b73b57d35113317eb9db980828
senichini <koko930325@gmail.com>
2025-04-01 01:08:57 +0800

    前端 - input元件、待審核畫面

--- 114408-front-end/components/common/InputField.tsx
+++ 114408-front-end/components/common/InputField.tsx
@@ -1,3 +1,43 @@
-export default function InputField(){
-    
+"use client";
+
+import styles from "@/styles/components/common/InputField.module.scss";
+import { UseFormRegisterReturn } from "react-hook-form";
+import React, { useState } from "react";
+
+export default function InputField({
+  type,
+  hint,
+  label,
+  register,
+  style,
+  icon,
+}: {
+  type: string;
+  hint?: string;
+  label?: string;
+  register?: UseFormRegisterReturn;
+  style?: React.CSSProperties;
+  icon?: React.ReactNode;
+}) {
+  const [hasValue, setHasValue] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasValue(e.target.value !== "");
+    register?.onChange?.(e);
+  };
+
+  return (
+    <div className={styles.fieldWrap}>
+      {label && <label className={styles.title}>{label}</label>}
+      {!hasValue && icon && <span className={styles.icon}>{icon}</span>}
+      <input
+        type={type}
+        placeholder={hint}
+        className={styles.field}
+        style={style}
+        {...register}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
--- 114408-front-end/src/app/user/page.tsx
+++ 114408-front-end/src/app/user/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
+import { Pencil } from "lucide-react";
+import { Search } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+import InputField from "@/components/common/InputField";
 import { liseData } from "@/lib/data/listData";
 import MobileListItem from "@/components/common/MobileListItem";
 import styles from "@/styles/app/UserPage.module.scss";
 
 export default function User() {
+  const { register, watch } = useForm();
+
+  const keyword = watch("keyword");
+  const onSearch = !!keyword;
+
   return (
     <div className={styles.wrap}>
-      <input type="text" name="" id="" />
+      <div className={styles.searchBar}>
+        <InputField
+          style={{ width: 250 }}
+          type="text"
+          icon={<Pencil size={20} />}
+          register={register("keyword")}
+        />
+        <Search
+          className={`${styles.searchBtn} ${onSearch ? styles.onSearch : ""}`}
+        />
+      </div>
       <div className={styles.list}>
         {liseData.map((item, index) => (
           <MobileListItem data={item} key={index} />
