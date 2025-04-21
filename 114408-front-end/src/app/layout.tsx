8a4c1275f5373009b354d9a0ece2503587516a3d
senichini <koko930325@gmail.com>
2025-04-21 12:00:15 +0800

    前端 - 登入畫面建置

--- 114408-front-end/src/app/layout.tsx
+++ 114408-front-end/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/main.scss";
 import type { Metadata } from "next";
+
 import BottomNav from "@/components/layout/BottomNav";
 
 export const metadata: Metadata = {
@@ -22,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <body>
-        <BottomNav title="待核銷報帳"/>
+        <BottomNav title="待核銷報帳" />
         {children}
       </body>
     </html>
