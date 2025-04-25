2fa0db41abae7bf391e178d9eaa8ce553fa1dadf
senichini <koko930325@gmail.com>
2025-04-25 15:24:34 +0800

    前端 - 新增userAPItype

---
+++ 114408-front-end/lib/types/UserAPIType.ts
@@ -0,0 +1,10 @@
+export type Register = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type Login = {
+  email: string;
+  password: string;
+};
--- 114408-front-end/services/userAPI.ts
+++ 114408-front-end/services/userAPI.ts
@@ -1,6 +1,13 @@
 import API from "@/lib/api/api";
 import { Response } from "@/lib/api/requestType";
+import { Login, Register } from "@/lib/types/UserAPIType";
 
 const userAPI = {
-  'register': (): Promise<Response<any>> => API.post(`/register`),
+  register: (data: Register): Promise<Response<any>> =>
+    API.post(`/register`, data, {
+      headers: { "Content-Type": "application/json" },
+    }),
+  login: (data: Login): Promise<any> => API.post("/login"),
 };
+
+export default userAPI;
