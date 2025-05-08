bcff08f5b315829df26fe92188211a31091ff613
senichini <koko930325@gmail.com>
2025-05-08 15:40:21 +0800

    前端 - 新增全域theme

--- 114408-front-end/lib/context/UserRoleContext.tsx
+++ 114408-front-end/lib/context/UserRoleContext.tsx
@@ -4,13 +4,16 @@ import { createContext, useContext, useEffect, useState } from "react";
 import userAPI from "@/services/userAPI";
 
 type Role = "admin" | "user" | null;
+type Theme = 0 | 1;
 
 interface UserRoleContextType {
   role: Role;
+  theme: number;
 }
 
 const UserRoleContext = createContext<UserRoleContextType>({
   role: null,
+  theme: 0,
 });
 
 export const useUserRole = () => useContext(UserRoleContext);
@@ -21,22 +24,28 @@ export const UserRoleProvider = ({
   children: React.ReactNode;
 }) => {
   const [role, setRole] = useState<Role>(null);
+  const [theme, setTheme] = useState<Theme>(0);
 
   useEffect(() => {
-    const fetchRole = async () => {
+    const fetch = async () => {
       try {
-        const res = await userAPI.getRole();
-        setRole(res.data);
+        const [res1, res2] = await Promise.all([
+          userAPI.getRole(),
+          userAPI.getTheme(),
+        ]);
+        setRole(res1.data);
+        setTheme(res2.data);
       } catch (err) {
         setRole(null);
+        setTheme(0);
       }
     };
 
-    fetchRole();
+    fetch();
   }, []);
 
   return (
-    <UserRoleContext.Provider value={{ role }}>
+    <UserRoleContext.Provider value={{ role, theme }}>
       {children}
     </UserRoleContext.Provider>
   );
